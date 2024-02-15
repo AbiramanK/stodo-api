44eba1abcd50d15a1b9068e2dc250666d1b2c819
Abiraman K <abiramancit@gmail.com>
2024-02-15 16:27:49 +0530

    feat(db): add users table migration

---
+++ src/main/resources/db/migration/mysql/V2__create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+  UNIQUE KEY (`email`)
+)
